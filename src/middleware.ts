0c39190686252d8275e9672799027da178709acc
Botnari Ion <ion.botnari@webitfactory.io>
2025-02-18 18:59:19 +0200

    middleware latest update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,26 +1,18 @@
-
 import { NextRequest, NextResponse } from "next/server";
-// import { getToken } from "next-auth/jwt";
-// const protectedRoutes = ["/user-info"];
- /**
-  * Next Auth system with middleware protection
-  * @param request comment provider and uncomment this for using middleware
-  * @returns 
-  */
-export default async function middleware(request: NextRequest) {
-//   const { pathname } = request.nextUrl;
-//   const isProtected = protectedRoutes.some((route) =>
-//     pathname.startsWith(route)
-//   );
+import { withAuth } from "next-auth/middleware";
 
-//   if (isProtected) {
-//     const token = await getToken({
-//       req: request,
-//       secret: process.env.AUTH_SECRET,
-//     });
-//     if (!token) {
-//       return NextResponse.redirect(new URL("/api/auth/signin", request.url));
-//     }
-//   }
-  return NextResponse.next();
-}
+/**
+ * Next Auth system with middleware protection
+ * @param request comment provider and uncomment this for using middleware
+ * @returns
+ */
+// export default withAuth(function middleware(request: NextRequest) {}, {
+//   callbacks: {
+//     authorized: ({ token }) => {
+//       return !!token;
+//     },
+//   },
+// });
+// export const config = {
+//   matcher: ["/user-info"],
+// };
